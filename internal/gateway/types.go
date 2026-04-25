01d1d6a1baa565f0ac83c2ae89df465f492c7e7c
Tobi Okedeji <tobiokedeji@gmail.com>
2026-04-25 20:00:55 +0100

    chore(sdk): remove Go and Python SDKs, clean dead gateway types

--- internal/gateway/types.go
+++ internal/gateway/types.go
@@ -5,35 +5,6 @@ import (
 	"time"
 )
 
-type RoutingStrategy int
-
-const (
-	StrategyCostOptimized RoutingStrategy = iota + 1
-	StrategyQualityFirst
-	StrategyLatencyFirst
-)
-
-func (s RoutingStrategy) String() string {
-	switch s {
-	case StrategyCostOptimized:
-		return "cost_optimized"
-	case StrategyQualityFirst:
-		return "quality_first"
-	case StrategyLatencyFirst:
-		return "latency_first"
-	default:
-		return "unknown"
-	}
-}
-
-type ProviderConfig struct {
-	Name     string
-	Endpoint string
-	Models   []string
-	Priority int
-	Timeout  time.Duration
-}
-
 type TokenUsage struct {
 	CageID       string
 	AssessmentID string
@@ -42,14 +13,6 @@ type TokenUsage struct {
 	InputTokens  int64
 	OutputTokens int64
 	Timestamp    time.Time
-}
-
-type BudgetStatus struct {
-	CageID         string
-	AssessmentID   string
-	TokenBudget    int64
-	TokensConsumed int64
-	Exhausted      bool
 }
 
 var (
